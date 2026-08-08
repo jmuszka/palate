d79fa9bc38c9afd6c74e175e83fea3a213341634
Josh Muszka <jmuszka@uwo.ca>
2026-08-08 12:16:35 -0400

    feat: enforce styling and linting

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
-import WordPage from './WordPage.tsx'
-import ComingSoon from './ComingSoon.tsx'
-import Layout from './Layout.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import WordPage from "./WordPage.tsx";
+import ComingSoon from "./ComingSoon.tsx";
+import Layout from "./Layout.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout><Outlet /></Layout>}>
+        <Route
+          element={
+            <Layout>
+              <Outlet />
+            </Layout>
+          }
+        >
           <Route path="/" element={<App />} />
           <Route path="/words/:word" element={<WordPage />} />
           <Route path="/about" element={<ComingSoon title="About" />} />
@@ -21,4 +27,4 @@ createRoot(document.getElementById('root')!).render(
       </Routes>
     </BrowserRouter>
   </StrictMode>,
-)
+);
