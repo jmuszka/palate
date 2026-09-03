79024f5ff660178f972d884853880e0e820fc467
Josh Muszka <jmuszka@uwo.ca>
2026-09-03 19:20:28 -0400

    fix: remove redundant date attribute

--- src/ChangelogPage.tsx
+++ src/ChangelogPage.tsx
@@ -37,9 +37,7 @@ export default function ChangelogPage() {
 
         {changelog.releases.map((release) => (
           <section key={release.version} className="flex flex-col gap-3">
-            <h3 className="text-zinc-800 text-lg font-semibold">
-              [{release.version}] - {release.date}
-            </h3>
+            <h3 className="text-zinc-800 text-lg font-semibold">[{release.version}]</h3>
             <p className="italic">{release.summary}</p>
             <div className="flex flex-col gap-1">
               <h4 className="text-sm font-medium text-zinc-700">Added</h4>
